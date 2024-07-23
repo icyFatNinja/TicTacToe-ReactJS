da7a28c5ed62697e3c98cecece2a293b47bbf349
icyFatNinja <nyuan0301@gmail.com>
2024-07-23 22:18:54 +0200

    update component files

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Board from "./Board";
 
 function TicTacToe() {
+  const [tiles, setTiles] = useState(Array(9).fill(null));
   return (
     <div>
       <h1>Tic Tac Toe</h1>
