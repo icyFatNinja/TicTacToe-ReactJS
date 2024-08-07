62b315467f4078fbc5ef91d08933da6fc447ecc2
icyFatNinja <nyuan0301@gmail.com>
2024-08-07 09:06:53 +0200

    update ticktactoe component file

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,8 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Board from "./Board";
 
 const PLAYER_X = "X";
 const PLAYER_O = "O";
+
+const winningCombinations = [
+  //Rows
+  { combo: [0, 1, 2], strikeClass: "strike-row-1" },
+  { combo: [3, 4, 5], strikeClass: "strike-row-2" },
+  { combo: [6, 7, 8], strikeClass: "strike-row-1" },
+  //Columns
+  { combo: [0, 3, 6], strikeClass: "strike-row-1" },
+  { combo: [1, 4, 7], strikeClass: "strike-row-1" },
+  { combo: [2, 5, 8], strikeClass: "strike-row-1" },
+  //Diagonals
+  { combo: [0, 4, 8], strikeClass: "strike-row-1" },
+  { combo: [2, 4, 6], strikeClass: "strike-row-1" },
+];
+
+function checkWinner(tiles, setStrikeClass) {
+  for (const { combo, strikeClass } of winningCombinations) {
+  }
+}
 
 function TicTacToe() {
   const [tiles, setTiles] = useState(Array(9).fill(null));
@@ -23,6 +42,10 @@ function TicTacToe() {
       setPlayerTurn(PLAYER_X);
     }
   };
+
+  useEffect(() => {
+    checkWinner();
+  }, [tiles]);
 
   return (
     <div>
