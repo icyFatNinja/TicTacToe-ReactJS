e4e7d2e96593624c401abdf751ecfa6bb20d81fd
icyFatNinja <nyuan0301@gmail.com>
2024-08-08 21:09:41 +0200

    update ticktactoe component file and css file and JS file

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import Board from "./Board";
+import GameOver from "./GameOver";
+import GameState from "./GameState";
 
 const PLAYER_X = "X";
 const PLAYER_O = "O";
@@ -8,18 +10,40 @@ const winningCombinations = [
   //Rows
   { combo: [0, 1, 2], strikeClass: "strike-row-1" },
   { combo: [3, 4, 5], strikeClass: "strike-row-2" },
-  { combo: [6, 7, 8], strikeClass: "strike-row-1" },
+  { combo: [6, 7, 8], strikeClass: "strike-row-3" },
   //Columns
-  { combo: [0, 3, 6], strikeClass: "strike-row-1" },
-  { combo: [1, 4, 7], strikeClass: "strike-row-1" },
-  { combo: [2, 5, 8], strikeClass: "strike-row-1" },
+  { combo: [0, 3, 6], strikeClass: "strike-column-1" },
+  { combo: [1, 4, 7], strikeClass: "strike-column-2" },
+  { combo: [2, 5, 8], strikeClass: "strike-column-3" },
   //Diagonals
-  { combo: [0, 4, 8], strikeClass: "strike-row-1" },
-  { combo: [2, 4, 6], strikeClass: "strike-row-1" },
+  { combo: [0, 4, 8], strikeClass: "strike-diagonal-1" },
+  { combo: [2, 4, 6], strikeClass: "strike-diagonal-2" },
 ];
 
-function checkWinner(tiles, setStrikeClass) {
+function checkWinner(tiles, setStrikeClass, setGameState) {
   for (const { combo, strikeClass } of winningCombinations) {
+    const tileValue1 = tiles[combo[0]];
+    const tileValue2 = tiles[combo[1]];
+    const tileValue3 = tiles[combo[2]];
+
+    if (
+      tileValue1 != null &&
+      tileValue1 === tileValue2 &&
+      tileValue1 === tileValue3
+    ) {
+      setStrikeClass(strikeClass);
+      if (tileValue1 === PLAYER_X) {
+        setGameState(GameState.playerXWins);
+      } else {
+        setGameState(GameState.playerOWins);
+      }
+      return; // it will not run check for draw when retrun.
+    }
+  }
+
+  const areAllTilesFilledIn = tiles.every((tile) => tile != null);
+  if (areAllTilesFilledIn) {
+    setGameState(GameState.draw);
   }
 }
 
@@ -27,8 +51,13 @@ function TicTacToe() {
   const [tiles, setTiles] = useState(Array(9).fill(null));
   const [playerTurn, setPlayerTurn] = useState(PLAYER_X);
   const [strikeClass, setStrikeClass] = useState();
+  const [gameState, setGameState] = useState(GameState.inProgress);
 
   const handleTileClick = (index) => {
+    if (gameState !== GameState.inProgress) {
+      return; // it will exit here and will not allow to click more when the game is over.
+    }
+
     if (tiles[index] != null) {
       return;
     }
@@ -44,7 +73,7 @@ function TicTacToe() {
   };
 
   useEffect(() => {
-    checkWinner();
+    checkWinner(tiles, setStrikeClass, setGameState);
   }, [tiles]);
 
   return (
@@ -56,8 +85,11 @@ function TicTacToe() {
         onTileClick={handleTileClick}
         strikeClass={strikeClass}
       />
+      <GameOver gameState={gameState} />
     </div>
   );
 }
 
 export default TicTacToe;
+
+// 45.05
